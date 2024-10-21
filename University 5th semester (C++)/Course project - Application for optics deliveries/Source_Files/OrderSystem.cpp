8e428be039f0012a252f32195944a9324a665639
vladislav-atanassov <121548043+vladislav-atanassov@users.noreply.github.com>
2024-10-21 15:38:24 +0300

    Update OrderSystem.cpp

--- University 5th semester (C++)/Course project - Application for optics deliveries/Source_Files/OrderSystem.cpp
+++ University 5th semester (C++)/Course project - Application for optics deliveries/Source_Files/OrderSystem.cpp
@@ -1,4 +1,4 @@
-#include "C:\Users\Vladislav Atanassov\Documents\Programming in university\University 5th semester (C++)\Course project - Application for optics deliveries\Header_files\OrderSystem.h"
+#include "your_path_to_directoryOrderSystem.h"
 
 void OrderSystem::displayDelivereres() const 
 {
