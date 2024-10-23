89cf3c1e88c684ecd66c01f5098a7d0d037558a9
vladislav-atanassov <121548043+vladislav-atanassov@users.noreply.github.com>
2024-10-23 11:09:20 +0300

    Update Optics.h

--- University 5th semester (C++)/Course project - Application for optics deliveries/Header_files/Optics.h
+++ University 5th semester (C++)/Course project - Application for optics deliveries/Header_files/Optics.h
@@ -35,14 +35,3 @@ public:
     void setDiopter(float diopter) { this->diopter = diopter; }
     void setPrice(float price) { this->price = price; }
 };
-
-// std::ostream& operator<<(std::ostream& os, const Optics& optics)
-// {
-//     os << "Optics Details:" << std::endl;
-//     os << "Type: " << optics.getType() << std::endl;
-//     os << "Material: " << optics.getMaterial() << std::endl;
-//     os << "Thickness: " << optics.getThickness() << " mm" << std::endl;
-//     os << "Diopter: " << optics.getDiopter() << std::endl;
-
-//     return os;
-// }
