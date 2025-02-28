c77e54f9e82d643d12f76254dc4287b541b52725
Vladislav Atanassov <vlavalat@gmail.com>
2025-02-28 22:20:08 +0200

    Notes for the second exercise

--- University 6th semester/System_Programming/upr_1/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    auto a  = +000001.5;
-
-    std::cout << typeid(a).name() << std::endl;
-    return 0;
-}
--- University 6th semester/System_Programming/upr_2/upr_2.c
+++ University 6th semester/System_Programming/upr_2/upr_2.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 50
+
+int read_file_to_buffer(const char *filepath, char *buffer, size_t buffer_size)
+{
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return -1;
+    }
+
+    size_t bytesRead = fread(buffer, 1, buffer_size - 1, file);
+    buffer[bytesRead] = '\0';
+    fclose(file);
+    return (int)bytesRead;
+}
 
 int main(int argc, char *argv[])
 {
+    char buffer[BUFFER_SIZE];
+
+    int bytesRead = read_file_to_buffer("example.txt", buffer, BUFFER_SIZE);
+    if (bytesRead >= 0)
+    {
+        printf("File contents:\n%s\n", buffer);
+    }
+    else
+    {
+        printf("Failed to read file.\n");
+    }
 
     return 0;
 }
